29a71733d56d248556aeda4d2ccb9a72fc3df75a
sushmaGit6281 <118921978+sushmaGit6281@users.noreply.github.com>
2022-12-14 01:08:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Header from './HeaderFile';
+import Form from './FormFile';
+function App() {
+  return (
+  <div className="App" >
+    <Header/>
+    <Form/>
+  </div>
+  )
+}
+
+export default App
